2ac40f3715d836e56ea31a91574efc87860fffda
Ruslan.Androsenko <ruslan.androsenko@leomax.ru>
2021-06-19 12:51:44 +0300

    Adding accessories to the test ver. 1.2.4

--- tests/MyClassTest.php
+++ tests/MyClassTest.php
@@ -6,20 +6,31 @@ use Src\MathException;
 
 class MyClassTest extends TestCase
 {
+    protected $fixture;
+
+    protected function setUp(): void
+    {
+        $this->fixture = new MyClass();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->fixture = NULL;
+    }
+
     /**
      * @dataProvider providerPower
      */
     public function testPower($a, $b, $c)
     {
-        $my = new MyClass();
-        $this->assertEquals($c, $my->power($a, $b));
+        $this->assertEquals($c, $this->fixture->power($a, $b));
     }
 
     public function providerPower()
     {
         return [
             [2, 2, 4],
-            [2, 3, 9],
+            [2, 3, 8],
             [3, 5, 243],
         ];
     }
@@ -28,16 +39,13 @@ class MyClassTest extends TestCase
     {
         $this->expectException(MathException::class);
 
-        $my = new MyClass();
-        $my->divide(8, 0);
+        $this->fixture->divide(8, 0);
     }
 
     public function testDivision2()
     {
-        $my = new MyClass();
-
         try {
-            $my->divide(8, 2);
+            $this->fixture->divide(8, 2);
         } catch (MathException $e) {
             return;
         }
