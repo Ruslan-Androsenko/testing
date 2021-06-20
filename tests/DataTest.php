79237f4e07b540148040d97658d6965456b5e029
Ruslan.Androsenko <ruslan.androsenko@leomax.ru>
2021-06-20 12:06:53 +0300

    Example 2.6 Using a data provider with datasets ver. 1.5.6

--- tests/DataTest.php
+++ tests/DataTest.php
@@ -17,11 +17,11 @@ class DataTest extends TestCase
     public function additionalProvider()
     {
         return [
-            [0, 0, 0],
-            [0, 1, 1],
-            [1, 0, 1],
-            [1, 1, 2],
-            // [1, 1, 3],
+            'adding zero' => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one' => [1, 1, 2],
+            // 'one plus one' => [1, 1, 3],
         ];
     }
 }
